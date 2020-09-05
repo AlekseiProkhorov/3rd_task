2b162467fe2ef994bb5402b32a2fb2fb3b4d15de
Alexey Prokhorov <alexeyprokhorov@outlook.com>
2020-09-05 15:03:10 +0300

    Initial commit

---
+++ src/ru/mirea/Main.java
@@ -0,0 +1,41 @@
+package ru.mirea;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int size = scanner.nextInt();
+        int[] nums = new int[size];
+        System.out.print("Enter elements: ");
+
+        for (int i = 0; i < size; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += nums[i];
+        }
+        System.out.println("For sum: " + sum);
+
+        int i = 0;
+        sum = 0;
+        while (i < size) {
+            sum += nums[i];
+            i++;
+        }
+        System.out.println("While sum: " + sum);
+
+        i = 0;
+        sum = 0;
+        do {
+            sum += nums[i];
+            i++;
+        } while (i < size);
+        System.out.println("While sum: " + sum);
+    }
+}
